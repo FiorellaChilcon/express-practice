513f35845523f720779f13eb4cf5d432df812d8d
fiorellachilcon <fiorellachilc_20@hotmail.com>
2023-10-25 16:39:41 -0500

    chore: set up cache strategy

--- src/infra/gateways/redis.ts
+++ src/infra/gateways/redis.ts
@@ -1,18 +1,37 @@
 import { env } from '@/main/config';
-import { RedisClientType, createClient } from 'redis';
+import { RedisClientType, SetOptions, createClient } from 'redis';
 
 export class RedisManager {
-  private static connection?: RedisClientType;
+  private static _client?: RedisClientType;
+
+  static getClient(): RedisClientType {
+    if (!RedisManager._client) {
+      const url = env.redis.url;
+      const redis = createClient({ url });
+      redis.on('error', (err) => console.log('Redis Client Error', err));
+      RedisManager._client = redis as RedisClientType;
+    }
+    return RedisManager._client;
+  }
 
   static async connect(): Promise<void> {
-    const url = env.redis.url;
-    const redis = createClient({ url });
-    redis.on('error', (err) => console.log('Redis Client Error', err));
+    const redis = RedisManager.getClient();
     await redis.connect();
-    this.connection = redis as RedisClientType;
   }
 
   static async disconnect(): Promise<void> {
-    if (RedisManager.connection) await RedisManager.connection.quit();
+    if (RedisManager._client) await RedisManager._client.quit();
+  }
+
+  static async get(key: string) {
+    return RedisManager.getClient().get(key);
+  }
+
+  static async set(key: string, data: string, options?: SetOptions) {
+    return RedisManager.getClient().set(key, data, options);
+  }
+
+  static async del(key: string) {
+    return RedisManager.getClient().del(key);
   }
 }
--- src/main/routes/users.ts
+++ src/main/routes/users.ts
@@ -1,17 +1,34 @@
+import express from 'express';
 import { DataValidationError, NotFoundError } from '@/domain/errors';
 import { User } from '@/domain/models';
 import { PgManager } from '@/infra/database';
 import { UserRepository } from '@/infra/repos';
-import express from 'express';
 import { udpateUserValidator } from '@/main/validators';
+import { RedisManager } from '@/infra/gateways';
+import { User as UserEntity } from '@/infra/entity';
+
 const usersRouter = express.Router();
+
+const ALL_USERS_CACHE_KEY = 'all_users';
+const ALL_USERS_CACHE_EXP = 60 * 60 * 24; // 1 day
 
 /* GET users listing. */
 usersRouter.get('/', async function (req, res, next) {
   try {
+    // Look data in cache
+    const usersCached = await RedisManager.get(ALL_USERS_CACHE_KEY);
+    if (usersCached) {
+      const cacheData = JSON.parse(usersCached) as UserEntity[];
+      return res.send({ data: cacheData.map(user => new User(user)), metadata: { cache: true } });
+    }
+
     const users = new UserRepository();
     const response = await users.findAll();
-    res.send(response.map(user => new User(user)));
+
+    // Save data in cache
+    await RedisManager.set(ALL_USERS_CACHE_KEY, JSON.stringify(response), { EX: ALL_USERS_CACHE_EXP });
+ 
+    res.send({ data: response.map(user => new User(user)), metadata: { cache: false } });
   } catch(err) {
     next(err);
   }
@@ -20,10 +37,24 @@ usersRouter.get('/', async function (req, res, next) {
 usersRouter.get('/:id', async function(req, res, next) {
   try {
     const userId = req.params.id;
+
+    // Look data in cache
+    const userCached = await RedisManager.get(userId);
+    if (userCached) {
+      const cacheData = JSON.parse(userCached) as UserEntity;
+      return res.send({ data: new User(cacheData), metadata: { cache: true } });
+    }
+
     const users = new UserRepository();
     const user = await users.findOneById(userId);
-    if (user) res.send(new User(user));
-    else throw new NotFoundError({ message: 'User not found' });
+    if (user) {
+      // Save data in cache
+      await RedisManager.set(userId, JSON.stringify(user));
+
+      return res.send({ data: new User(user), metadata: { cache: false } });
+    } else {
+      throw new NotFoundError({ message: 'User not found' });
+    }
   } catch(err) {
     next(err);
   }
@@ -48,7 +79,10 @@ usersRouter.put('/:id', async function(req, res, next) {
     });
   
     if (result) {
-      res.send(new User(result));
+      // Save/Update data in cache
+      await RedisManager.set(userId, JSON.stringify(result));
+
+      return res.send(new User(result));
     } else {
       throw new NotFoundError({ message: 'User not found' });
     }
@@ -69,7 +103,10 @@ usersRouter.delete('/:id', async function(req, res, next) {
     });
 
     if (result) {
-      res.send(new User(result));
+      // Remove data in cache
+      await RedisManager.del(userId);
+
+      return res.send(new User(result));
     } else {
       throw new NotFoundError({ message: 'User not found' });
     }
